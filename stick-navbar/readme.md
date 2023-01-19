c6613c3a955b2e5b15975488dfce4fd0efa3d16e
jonathas-silva <silva.jonathas@outlook.com>
2023-01-19 20:27:55 -0300

    projeto stick-navbar

---
+++ stick-navbar/readme.md
@@ -0,0 +1,3 @@
+### STICK NAVBAR
+
+Projeto reutilizável de navbar responsiva, utilizando o componente já criado de *dropdown*, com uma bonita expansão do dropdown quando o menu é expandido em telas pequenas.
---
+++ stick-navbar/stick-navbar.js
@@ -0,0 +1,7 @@
+let botoes = document.getElementById('buttons-group');
+let nav = document.getElementById('nav-principal');
+
+function openMenu() {
+   nav.classList.toggle("toggle-nav");
+   botoes.classList.toggle("toogle-buttons");
+}
